ba301415a60295aec8c112a20e40815e73533bb0
Andrew <adharris@gmail.com>
2013-08-20 16:43:25 -0400

    forgot application loop

--- dyb/recipes/default.rb
+++ dyb/recipes/default.rb
@@ -1,8 +1,10 @@
 include_recipe "python"
 
-python_virtualenv "#{deploy[:deploy_to]}/venv" do
-	interpreter "2.7.4"
-	owner deploy[:user]
-	group deploy[:group]
-	action :create
+node[:deploy].each do |application, deploy| do
+	python_virtualenv "#{deploy[:deploy_to]}/venv" do
+		interpreter "2.7.4"
+		owner deploy[:user]
+		group deploy[:group]
+		action :create
+	end
 end
