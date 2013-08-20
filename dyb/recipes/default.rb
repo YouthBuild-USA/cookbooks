a00fcd96f1a8cd178953c3ab47b778cfd619edd1
Andrew <adharris@gmail.com>
2013-08-20 16:49:30 -0400

    I dont know ruby

--- dyb/recipes/default.rb
+++ dyb/recipes/default.rb
@@ -1,6 +1,6 @@
 include_recipe "python"
 
-node[:deploy].each do |application, deploy| do
+node[:deploy].each do |application, deploy|
 	python_virtualenv "#{deploy[:deploy_to]}/venv" do
 		interpreter "2.7.4"
 		owner deploy[:user]
